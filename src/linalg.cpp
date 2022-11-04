89a34f3827f5d0f7894f323ef60ef39b849dac3c
myerspat <myerspat@umich.edu>
2022-11-03 21:56:26 -0400

    Moved matrix specific functions and created COO matrix class

--- src/linalg.cpp
+++ src/linalg.cpp
@@ -1,17 +1,12 @@
-#include "./mesh.hpp"
-#include <utility>
+#include "linalg.hpp"
 
 namespace linalg {
 
-std::pair<double *, double> powerIteration(Mesh mesh) {}
+std::pair<std::vector<double>, double> powerIteration(Mesh mesh) {}
 
 double norm(double *vec) {}
 
-double *matvec(double **mat, double *vec) {}
-
 double dot(double *vec1, double *vec2) {}
-
-double *vecmat(double *vec, double **mat) {}
 
 double **inv(double **mat) {}
 
