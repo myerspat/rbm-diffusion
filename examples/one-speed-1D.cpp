88653932573ecb5750f9b9a4b960ef1f1fcf515d
myerspat <myerspat@umich.edu>
2022-11-09 21:20:06 -0500

    finished 1d 1 speed problem

--- examples/one-speed-1D.cpp
+++ examples/one-speed-1D.cpp
@@ -6,6 +6,8 @@
 #include <cstdio>
 #include <stdexcept>
 #include <string>
+#include <utility>
+#include <fstream>
 #include <vector>
 
 // 1D one-speed example with uniform slab reactor with reflective boundary
@@ -35,9 +37,17 @@ int main(int argc, char *argv[]) {
   double Sigma_a_t = std::stod(argv[2]);
 
   // Training points
-  std::vector<double> Sigma_a_train = {0.02, 0.04, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18};
+  // std::vector<double> Sigma_a_train = {0.02, 0.04, 0.08, 0.10, 0.12, 0.14, 0.16, 0.18};
+  std::vector<double> Sigma_a_train = {0.08, 0.10, 0.12, 0.14};
   std::vector<std::vector<double> > flux_train;
   std::vector<double> k_train;
+
+  printf("For the following target parameter: Absorption XS\n");
+  printf("The training points are: ");
+  for (size_t i = 0; i < Sigma_a_train.size(); i++) {
+    printf("%lg ", Sigma_a_train[i]);
+  }
+  printf("\n\n");
 
   flux_train.reserve(Sigma_a_train.size());
   k_train.reserve(Sigma_a_train.size());
@@ -48,12 +58,10 @@ int main(int argc, char *argv[]) {
   double nu_Sigma_f = 0.11;     // cm^-1
 
   // Boundaries
-  std::pair<double, double> left_bound;  // a/b left boundary condition
-  std::pair<double, double> right_bound; // a/b right boundary condition
-  left_bound.first = 0.0;                // a
-  left_bound.second = 1.0;               // b
-  right_bound.first = 1.0;               // a
-  right_bound.second = -2.0;              // b
+  std::pair<double, double> left_bound = std::make_pair(0.0, 1.0);   // a/b left boundary condition
+  std::pair<double, double> right_bound = std::make_pair(1.0, -2.0); // a/b right boundary condition
+
+  printf("Calculating training eigenvectors/values\n");
 
   // Iterate over each training point
   for (double &Sigma_a : Sigma_a_train) {
@@ -63,53 +71,62 @@ int main(int argc, char *argv[]) {
     my_mesh.run(nu_Sigma_f, Sigma_a, D);
 
     // Initializing M, F into variable (could be done in powerIteration)
-    linalg::Matrix M = my_mesh.getF();
-    linalg::Matrix F = my_mesh.getM();
+    linalg::Matrix M = my_mesh.getM();
+    linalg::Matrix F = my_mesh.getF();
 
     // Run power iteration to solve eigenvalue problem
     auto eigen_result = linalg::powerIteration(M, F);
     flux_train.push_back(eigen_result.first);
     k_train.push_back(eigen_result.second);
+    printf("k = %5lg\n", k_train.back());
   }
+
+  printf("\nCalculating target value %lg\n", Sigma_a_t);
 
   // Initialize target matricies
   linalg::Matrix M_t(Sigma_a_train.size(), Sigma_a_train.size());
   linalg::Matrix F_t(Sigma_a_train.size(), Sigma_a_train.size());
 
-  // Build target matricies
-  for (size_t i = 0; i < Sigma_a_train.size(); i++) {
-    for (size_t j = 0; j < Sigma_a_train.size(); j++) {
-      // Intermediate vector-matrix multiplication for adjoint flux
-      std::vector<double> M_t_inter = M_t.vecmat(flux_train[i]);
-      std::vector<double> F_t_inter = F_t.vecmat(flux_train[i]);
-
-      // Set the dot of previous result and forward flux
-      M_t.set(i, j, linalg::dot(M_t_inter, flux_train[j]));
-      F_t.set(i, j, linalg::dot(F_t_inter, flux_train[j]));
-    }
-  }
-
-  // Run power iteration to solve generalized eigenvalue problem
-  auto target = linalg::powerIteration(M_t, F_t);
-  double k_t = target.second;
-
-  // Run finite difference to get "exact" solution
-  // USE MESH CONSTROCTOR HERE
-
   // Making Mesh object and getting M, F matricies for target parameter
   Mesh my_mesh(N, half_thickness, left_bound, right_bound);
   my_mesh.run(nu_Sigma_f, Sigma_a_t, D);
-  linalg::Matrix M = my_mesh.getF();
-  linalg::Matrix F = my_mesh.getM();
+  linalg::Matrix M = my_mesh.getM();
+  linalg::Matrix F = my_mesh.getF();
 
-  auto exact = linalg::powerIteration(M_t, F_t);
+  // Run finite difference to get "exact" solution
+  auto exact = linalg::powerIteration(M, F);
   double k_t_exact = exact.second;
+  printf("k_exact = %5lg\n", k_t_exact);
 
-  // Calculate relative error
-  double rel_err = std::abs(k_t - k_t_exact) / k_t_exact;
+  // Build target matricies
+  for (size_t i = 0; i < Sigma_a_train.size(); i++) {
+    for (size_t j = 0; j < Sigma_a_train.size(); j++) {
+      // Set the dot of previous result and forward flux
+      M_t.set(i, j, linalg::dot(M.vecmat(flux_train[i]), flux_train[j]));
+      F_t.set(i, j, linalg::dot(F.vecmat(flux_train[i]), flux_train[j]));
+    }
+  }
 
-  // Print results
-  printf("For target Sigma_a = %5lg, k_t = %6lg, with %6lg relative error.", Sigma_a_t, k_t, rel_err);
+  // // Run power iteration to solve generalized eigenvalue problem
+  // auto target = linalg::powerIteration(M_t, F_t);
+  // double k_t = target.second;
+  //
+  // // Calculate relative error
+  // double rel_err = std::abs(k_t - k_t_exact) / k_t_exact;
+  //
+  // // Print results
+  // printf("For target Sigma_a = %5lg, k_t = %6lg, with %6lg relative error.", Sigma_a_t, k_t, rel_err);
+  
+  std::ofstream f("training_fluxes.csv");
+
+  for (auto& training_flux : flux_train) {
+    for (auto& flux : training_flux) {
+      f << flux << ", ";
+    }
+    f << "\n";
+  }
+
+  f.close();
 
   return 0;
 }
