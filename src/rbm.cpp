53bfb6e9b4cf4ac766536beaccf8c0677fb3b041
burrelle <burrelle@umich.edu>
2022-11-19 23:24:59 -0500

    started construct of F_t and M_t

--- src/rbm.cpp
+++ src/rbm.cpp
@@ -1,6 +1,7 @@
 #include "rbm/rbm.hpp"
 #include "xtensor/xtensor_forward.hpp"
 #include <utility>
+#include "xtensor-blas/xlinalg.hpp"
 
 namespace rbm {
 
@@ -15,6 +16,19 @@ xt::xarray<double> RBM::constructF_t(
   xt::xarray<double> F_t = xt::xarray<double>::from_shape(
     {training_fluxes.shape(0), training_fluxes.shape(0)});
 
+    
+
+    for (size_t i = 0; i < training_fluxes.shape(0); i++) {
+      // get row and coln fluxes 
+      auto flux_i = xt::view(training_fluxes, i, xt::all());
+      auto flux_j = xt::view(training_fluxes, i, xt::all());
+      // find F_t
+      xt::row(F_t, i) = xt::linalg::dot(flux_i, xt::linalg::dot(F, flux_j));
+    }
+
+    // for 0, M-1
+    // Ft(i :)=fluxs_row(i) *[ F * fluxs_coln(i) ] 
+
   return F_t;
 }
 
@@ -25,6 +39,16 @@ xt::xarray<double> RBM::constructM_t(
   xt::xarray<double> M_t = xt::xarray<double>::from_shape(
     {training_fluxes.shape(0), training_fluxes.shape(0)});
 
+    for (size_t i = 0; i < training_fluxes.shape(0); i++) {
+      // get row and coln fluxes 
+      auto flux_i = xt::view(training_fluxes, i, xt::all());
+      auto flux_j = xt::view(training_fluxes, i, xt::all());
+      // find M_t
+      xt::row(M_t, i) = xt::linalg::dot(flux_i, xt::linalg::dot(M, flux_j));
+    }
+
+    
+
   return M_t;
 }
 
@@ -32,7 +56,40 @@ void PerturbAbsorption::initialize(
   xt::xarray<double>& training_points, mesh::Mesh& mesh, int& cell_id)
 {}
 
-void PerturbAbsorption::train() {}
+void PerturbAbsorption::train() {
+
+// full training_fluxes and training_k (which is 1/egienvalue)
+
+//auto flux = xt::view(flux_train, i, xt::all());
+//flux = xt::real(xt::col(std::get<1>(eigenfunction), 0)) * -1;
+
+// for i = 0, training points.shape(0)-1
+// change Cell( "abosoption", _traning points(i),cellid)
+// F=constructF() (nxn)
+// M=constructM() (nxn)
+//xt::xarray<double> M = my_mesh.getM();
+//  xt::xarray<double> F = my_mesh.getF();
+//eig(M^(-1) *F  //tuple eigenvecotors and eigencetor (want one evigitveotor and cospiong eigvalue)
+
+//fill traing flux / k
+//pcareduce() (dont use for unit test)
+
+//xt::xarray<double> A = xt::linalg::dot(xt::linalg::inv(M), F);
+// auto eigenfunction = xt::linalg::eig(A);
+//k_train[i] = std::get<0>(eigenfunction)(0, 0).real();
+
+// // Build target matricies
+//   for (size_t i = 0; i < Sigma_a_train.size(); i++) {
+//     for (size_t j = 0; j < Sigma_a_train.size(); j++) {
+//       // Set the dot of previous result and forward flux
+//       auto flux_i = xt::view(flux_train, i, xt::all());
+//       auto flux_j = xt::view(flux_train, i, xt::all());
+//       M_t(i, j) = xt::linalg::dot(flux_i, xt::linalg::dot(M, flux_j))(0, 0);
+//       F_t(i, j) = xt::linalg::dot(flux_i, xt::linalg::dot(F, flux_j))(0, 0);
+//     }
+//   }
+
+}
 
 std::pair<xt::xarray<double>, double> PerturbAbsorption::calcTarget(
   double target_value)
--- tests/test_rbm.cpp
+++ tests/test_rbm.cpp
@@ -1,4 +1,37 @@
 #include "rbm/rbm.hpp"
 #include "unit_test_framework.hpp"
 
+
+TEST(ContructF_t_test)
+{
+ xt::xarray<double> F = xt::xarray<double>::from_shape(
+    {3, 3});
+
+    F(0,0)=2.2;
+    F(1,0)=0.0;
+    F(2,0)=0.0;
+    F(0,1)=0.0;
+    F(1,1)=1.3;
+    F(2,1)=0.0;
+    F(0,2)=0.0;
+    F(1,2)=0.0;
+    F(2,2)=3.5;  
+
+ xt::xarray<double> Flux = xt::xarray<double>::from_shape(
+    {2, 3});
+    
+    Flux(0,0)=0.2;
+    Flux(1,0)=0.94;
+    Flux(0,1)=0.03;
+    Flux(1,1)=0.3;
+    Flux(0,2)=0.230;
+    Flux(1,2)=0.540;
+
+  xt::xarray<double> F_t =  RBM::constructF_t(F, Flux);
+    
+
+}
+
+
+
 TEST_MAIN();
