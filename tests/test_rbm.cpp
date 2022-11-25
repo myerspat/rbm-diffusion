3e7b867bcfea577f440a3667197adfdb68b2651c
burrelle <burrelle@umich.edu>
2022-11-25 14:13:59 -0500

    updated F_t test

--- tests/test_rbm.cpp
+++ tests/test_rbm.cpp
@@ -18,21 +18,21 @@ TEST(ContructF_t_test)
     F(2,2)=3.5;  
 
  xt::xarray<double> Flux = xt::xarray<double>::from_shape(
-    {2, 3});
+    {3, 2});
     
     Flux(0,0)=0.2;
     Flux(1,0)=0.94;
-    Flux(0,1)=0.03;
-    Flux(1,1)=0.3;
-    Flux(0,2)=0.230;
-    Flux(1,2)=0.540;
+    Flux(2,0)=0.03;
+    Flux(0,1)=0.3;
+    Flux(1,1)=0.230;
+    Flux(2,1)=0.540;
 
  // xt::xarray<double> F_t =  rbm::RBM::constructF_t(F, Flux);
 //  rbm::RBM::constructF_t(F, Flux);
    rbm::PerturbAbsorption object;
    xt::xarray<double> F_t = object.constructF_t(F, Flux);
 
-   ASSERT_ALMOST_EQUAL(F_t(0, 0), 1.2398, 0.001);
+   ASSERT_ALMOST_EQUAL(F_t(0, 0), 1.2398, 0.0001);
 
 //    for (size_t i : I) {
 //     for (size_t j : J) {
