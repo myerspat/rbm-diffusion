98d129d9a7ce8968c7a3be6c336a16e1a612b3ff
myerspat <myerspat@umich.edu>
2022-11-28 15:41:06 -0500

    skeleton function

--- src/material.cpp
+++ src/material.cpp
@@ -1,5 +1,9 @@
 #include "rbm/material.hpp"
 
-void Material::initialize(std::string name, double absorption, double nu_fission, double D) {
+void Material::initialize(
+  std::string name, double absorption, double nu_fission, double D)
+{}
 
-}
+void Material::setParameter(
+  const double& new_value, const rbm::Parameter target_parameter)
+{}
