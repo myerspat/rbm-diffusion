a1d96e7989c591df1b92022b5bf5c39a93263431
myerspat <myerspat@umich.edu>
2022-11-28 21:57:20 -0500

    formatting

--- src/rbm.cpp
+++ src/rbm.cpp
@@ -1,10 +1,9 @@
 #include "rbm/rbm.hpp"
 #include "xtensor-blas/xlinalg.hpp"
 #include "xtensor/xtensor_forward.hpp"
-#include <utility>
-#include "xtensor-blas/xlinalg.hpp"
-#include <tuple>
 #include <string>
+#include <tuple>
+#include <utility>
 
 namespace rbm {
 
@@ -58,34 +57,34 @@ void Perturb::initialize(
   xt::xarray<double>& training_points, mesh::Mesh& mesh, size_t& element_id)
 {}
 
-void Perturb::train() {
-// full training_fluxes and training_k (which is 1/egienvalue)
-  xt::xarray<double> training_fluxes = xt::xarray<double>::from_shape({_mesh.getSize(), _training_points.shape(0)});
+void Perturb::train()
+{
+  // full training_fluxes and training_k (which is 1/egienvalue)
+  xt::xarray<double> training_fluxes = xt::xarray<double>::from_shape(
+    {_mesh.getSize(), _training_points.shape(0)});
 
-  xt::xarray<double> training_k = xt::xarray<double>::from_shape({_training_points.shape(0)});
+  xt::xarray<double> training_k =
+    xt::xarray<double>::from_shape({_training_points.shape(0)});
 
-  for (size_t i = 0; i < _training_points.shape(0); i++){
+  for (size_t i = 0; i < _training_points.shape(0); i++) {
     // set parameter
     std::string absor = "absorption";
-    _mesh.changeCell(_cell_id, absor,  _training_points(i));
+    _mesh.changeCell(_cell_id, absor, _training_points(i));
     // find F and M matrices
     xt::xarray<double> F = _mesh.constructF(); //(nxn)
-    xt::xarray<double> M =  _mesh.constructM(); //(nxn)
-    // find eigenvalues and eigenvectors 
+    xt::xarray<double> M = _mesh.constructM(); //(nxn)
+    // find eigenvalues and eigenvectors
     xt::xarray<double> A = xt::linalg::dot(xt::linalg::inv(M), F);
     auto eigenfunction = xt::linalg::eig(A);
-    training_k(i) = 1/(std::get<0>(eigenfunction)(0).real());
+    training_k(i) = 1 / (std::get<0>(eigenfunction)(0).real());
     xt::col(training_fluxes, i) = std::get<1>(eigenfunction)(0).real();
-
   }
 
   // reduce to PxP
-  pcaReduce(training_fluxes,training_k);
-
+  pcaReduce(training_fluxes, training_k);
 }
 
-std::pair<xt::xarray<double>, double> Perturb::calcTarget(
-  double target_value)
+std::pair<xt::xarray<double>, double> Perturb::calcTarget(double target_value)
 {
   xt::xarray<double> target_flux =
     xt::xarray<double>::from_shape({_mesh.getSize()});
