d07840a48919f2ddac8d5105b5179500e7734a86
connorcr <connorcr@umich.edu>
2022-11-30 21:48:11 -0500

    More robust test of setParameter in material

--- tests/test_material.cpp
+++ tests/test_material.cpp
@@ -14,7 +14,7 @@ TEST(test_Material_1)
 }
 TEST(test_setParameters)
 {
-  //Contructing Material 
+  // Contructing Material
   Material mat("fuel", 1, 2, 3);
   // Setting new value for absorption
   double new_value = 10;
@@ -27,15 +27,18 @@ TEST(test_setParameters)
   target_type = rbm::Parameter::nu_fission;
   new_value = 11;
   mat.setParameter(new_value, target_type);
-  //checking if nu fission cahnged
+  // checking if nu fission cahnged
   ASSERT_EQUAL(mat.getNuFission(), 11);
+  ASSERT_EQUAL(mat.getD(), 3);
+  ASSERT_EQUAL(mat.getAbsorption(), 10);
 
   // Checking new value for D
   target_type = rbm::Parameter::D;
   new_value = 12;
   mat.setParameter(new_value, target_type);
   ASSERT_EQUAL(mat.getD(), 12);
-
+  ASSERT_EQUAL(mat.getNuFission(), 11);
+  ASSERT_EQUAL(mat.getAbsorption(), 10);
 }
 
 TEST_MAIN();
